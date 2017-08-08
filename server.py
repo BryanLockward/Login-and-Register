dfa7d2172b70fc7f2b9a62697ff8eb8ef9210718
Bryan Lockward <bmlockward3@verizon.net>
2017-08-07 22:12:58 -0400

    The Wall

--- server.py
+++ server.py
@@ -1,76 +1,253 @@
-from flask import Flask, render_template, request, redirect, flash, session
-#mysql import
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 from mysqlconnection import MySQLConnector
-from flask_bcrypt import Bcrypt
+from flask.ext.bcrypt import Bcrypt
+import re
+import pprint
+from itertools import groupby
+
 app = Flask(__name__)
-app.secret_key = "secret_stuff"
 bcrypt = Bcrypt(app)
-mysql = MySQLConnector(app, 'login')
+mysql = MySQLConnector(app,'wall')
+app.secret_key = 'secret'
+
+emailRegex = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+passwordRegex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$')
+
+
+def validate():
+    errors = 0
+    if request.form['first_name'] == '':
+        flash('Name cannot be blank', 'first_nameError')
+        errors += 1
+        pass
+    elif any(char.isdigit() for char in request.form['first_name']) == True:
+        flash('Name cannot have numbers', 'first_nameError')
+        errors += 1
+        pass
+    else:
+        session['first_name'] = str(request.form['first_name'])
+
+    if request.form['last_name'] == '':
+        flash('Name cannot be blank', 'last_nameError')
+        errors += 1
+        pass
+    elif any(char.isdigit() for char in request.form['last_name']) == True:
+        flash('Name cannot have numbers', 'last_nameError')
+        errors += 1
+        pass
+    else:
+        session['last_name'] = str(request.form['last_name'])
+
+    if str(request.form['email']) == '':
+        flash('Email cannot be blank', 'emailError')
+        errors += 1
+        pass
+    elif not emailRegex.match(request.form['email']):
+        flash('Invalid email address', 'emailError')
+        errors += 1
+        pass
+    else:
+        session['email'] = str(request.form['email'])
+
+    if str(request.form['password']) == '':
+        flash('Password cannot be blank', 'passwordError')
+        errors += 1
+        pass
+    elif len(request.form['password']) < 8:
+        flash('Password must be greater than 8 characters', 'passwordError')
+        errors += 1
+        pass
+    elif not passwordRegex.match(request.form['password']):
+        flash('Password must contain at least one lowercase letter, one uppercase letter, and one digit', 'passwordError')
+        errors += 1
+        pass
+    else:
+        session['password'] = str(request.form['password'])
+
+    if str(request.form['confirm_password']) == '':
+        flash('Please confirm password', 'confirm_passwordError')
+        errors += 1
+        pass
+    elif str(request.form['confirm_password']) != str(request.form['password']):
+        flash('Passwords do not match', 'confirm_passwordError')
+        errors += 1
+    else:
+        session['confirm_password'] = str(request.form['confirm_password'])
+
+    if errors > 0:
+        session['password'] = ''
+        session['confirm_password'] = ''
+        return False
+    else:
+        return True
+
+def validateLogin():
+    errors = 0
+    if str(request.form['email']) == '':
+        flash('Email cannot be blank', 'loginEmailError')
+        errors += 1
+        pass
+    elif not emailRegex.match(request.form['email']):
+        flash('Invalid email address', 'loginEmailError')
+        errors += 1
+        pass
+    else:
+        session['email'] = str(request.form['email'])
+
+    if request.form['password'] == '':
+        flash('Password cannot be blank', 'loginPasswordError')
+        errors += 1
+        pass
+    else:
+        session['password'] = str(request.form['password'])
+
+    if errors > 0:
+        session['password'] = ''
+        return False
+    else:
+        return True
+
+def setUserId():
+    getUserId = "SELECT idusers FROM users WHERE email = '{}'".format(session['email'])
+    id = mysql.query_db(getUserId)
+    session['userid'] = id[0]['idusers']
+    return True
+
+def checkIfEmailExists():
+    getEmails = "SELECT email FROM users WHERE email = '{}'".format(session['email'])
+    emailList = mysql.query_db(getEmails)
+    if emailList == None:
+        return False
+    else:
+        return True
+
+def getPostsWithUsers():
+    getPosts = "SELECT posts.idposts, posts.post_text, posts.created_at, users.idusers, users.idusers, users.first_name, users.last_name FROM posts LEFT JOIN users ON posts.user_id = users.idusers ORDER BY posts.created_at DESC"
+    posts = mysql.query_db(getPosts)
+    return posts
+
+def getComments():
+    getComments = "SELECT comments.post_id, comments.comment_text, comments.created_at, users.first_name, users.last_name FROM comments LEFT JOIN posts ON comments.post_id = posts.idposts LEFT JOIN users ON comments.user_id = users.idusers"
+    comments = mysql.query_db(getComments)
+    return comments
+
+def getPostsWithComments():
+    comments = getComments()
+    posts = getPostsWithUsers()
+    commentList = {}
+    postsWithCommentContainer = []
+
+    for comment in comments:
+        info = {
+                "post_id": comment['post_id'],
+                "created_at": comment['created_at'].strftime("%B %d, %Y %-I:%M %p" ),
+                "first_name": comment['first_name'],
+                "last_name": comment['last_name'],
+                "comment_text": comment['comment_text']
+            }
+        if comment['post_id'] in commentList:
+            commentList[comment['post_id']].append(info)
+        else:
+            commentList[comment['post_id']] = [info]
+
+    for post in posts:
+        postData = {
+            "created_at": post['created_at'].strftime("%B %d, %Y %-I:%M %p" ),
+            "post_id": post['idposts'],
+            "first_name": post['first_name'],
+            "last_name": post['last_name'],
+            "post_text": post['post_text'],
+            "new_id": post['created_at'].strftime("%B%d%Y%-I%M%s" )
+        }
+
+        if post['idposts'] in commentList:
+            postData['comments'] = commentList[post['idposts']]
+        postsWithCommentContainer.append(postData)
+
+    return postsWithCommentContainer
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/sucess',methods=["POST"])
-def success():
-    input_password=request.form['password']
-    query = "SELECT * FROM users WHERE email = :email"
-    data = {
-        'email': request.form['email']
-    }
-    user= mysql.query_db(query,data)
-
-    if user:
-        for x in user:
-            if bcrypt.check_password_hash(x['password'],input_password):
-                return render_template("sucess.html")
-            else:
-                flash("Incorrect Password")
-                return redirect("/")
-    else:
-        flash("Incorrect Email")
-        return redirect("/")
-
-
-
-
-@app.route('/register', methods=["POST"])
-def register():
-    query = "SELECT * FROM users WHERE email = :email"
-    data = {
-        'email': request.form['email']
-    }
-    results = mysql.query_db(query,data) # [] , [{}]
-
-    if len(results) > 0:
-        flash('Email already in use')
-        return redirect('/')
-    elif len(request.form['first_name'])<2:
-        flash("First Name must be more than 2 charachters")
-        return redirect('/')
-    elif len(request.form['last_name'])<2:
-        flash("Last Name must be more than 2 charachters")
+@app.route('/create', methods=['POST'])
+def create():
+    if validate() == False:
+        session['loggedin'] = False
         return redirect('/')
     else:
-        if len(request.form['password']) < 8:
-            flash('password not long enough')
-            return redirect('/')
-        elif request.form['password'] !=  request.form['password_confirmation']:
-            flash("Passwords Dont Match")
-            return redirect('/')
+        if checkIfEmailExists() == True:
+            encryptedPassword = bcrypt.generate_password_hash(request.form['password'])
+            query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES ('{}', '{}', '{}','{}', NOW(), NOW())".format(session['first_name'], session['last_name'], session['email'], encryptedPassword)
+            mysql.run_mysql_query(query)
+            session['password'] = ''
+            session['confirm_password'] = ''
+            session['loggedin'] = True
+            return redirect('/dashboard')
         else:
-            enc_password = bcrypt.generate_password_hash(request.form['password'])
-            query = "INSERT INTO users (email, password, first_name,last_name,created_at, updated_at) VALUES (:email, :password,:first_name,:last_name, NOW(), NOW())"
-            data = {
-                'email': request.form['email'],
-                'password': enc_password,
-                'first_name':request.form['first_name'],
-                'last_name':request.form['last_name']
+            flash('Account with email already exists. Please use another email', 'emailError')
+    return redirect('/')
 
-
-            }
-            mysql.query_db(query,data)
-            flash("Succesful Registration, Please login")
+@app.route('/validate', methods=['POST'])
+def validateLoginInfo():
+    if validateLogin() == False:
+        session['loggedin'] = False
+        return redirect('/login')
+    else:
+        userInfo = mysql.query_db("SELECT * FROM users WHERE email = '{}'".format(session['email']))
+        if userInfo:
+            inputPassword = request.form['password']
+            if bcrypt.check_password_hash(userInfo[0]['password'], inputPassword):
+                session['loggedin'] = True
+                session['userid'] = userInfo[0]['idusers']
+                session['first_name'] = userInfo[0]['first_name']
+                return redirect('/dashboard')
+            else:
+                flash('Incorrect password', 'loginPasswordError')
+        else:
+            flash('No user with that email exists. Please create new user', 'loginEmailError')
             return redirect('/')
+    return redirect('/dashboard')
+
+@app.route('/dashboard')
+def returnDashboard():
+    if session['loggedin'] == True:
+        setUserId()
+        postData = getPostsWithComments()
+        return render_template('sucess.html', posts=postData)
+    else:
+        return redirect('/')
+
+@app.route('/post', methods=['POST'])
+def postData():
+    if session['loggedin'] == True:
+        postMessage = str(request.form['post'])
+        query = "INSERT INTO messages (post_text, created_at, updated_at, user_id) VALUES ('{}', NOW(), NOW(), '{}')".format(postMessage, session['userid'])
+        mysql.run_mysql_query(query)
+        return redirect('/dashboard')
+    else:
+        return redirect('/')
+
+@app.route('/post/<post_id>/comment', methods=['POST'])
+def postComment(post_id):
+    if session['loggedin'] == True:
+        commentMessage = str(request.form['comment'])
+        query = "INSERT INTO comments (post_id, user_id, comment_text, created_at, updated_at) VALUES ('{}', '{}', '{}', NOW(), NOW())".format(post_id, session['userid'], commentMessage)
+        mysql.run_mysql_query(query)
+        return redirect('/dashboard')
+    else:
+        return redirect('/')
+
+@app.route('/logout')
+def clear():
+    session['first_name'] = ''
+    session['last_name'] = ''
+    session['email'] = ''
+    session['password'] = ''
+    session['confirm_password'] = ''
+    session['userid'] = ''
+    session['loggedin'] = False
+
+    return redirect('/')
 
 app.run(debug=True)
